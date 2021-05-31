e0811971728ebd3b455655cb331faa1b95e221d3
Humberto Hernández <sasori250393@gmail.com>
2021-05-30 20:43:46 -0500

    Segundo release

--- src/configuration/ApiConfiguration.js
+++ src/configuration/ApiConfiguration.js
@@ -3,5 +3,5 @@ export const CONFIG_FETCH_GET = {
 	method: 'GET'
 };
 export const API_KEY_FILMS = '43172ad';
-export const API_POSTER_FILMS_URL = 'http://www.omdbapi.com/?t=';
+export const API_POSTER_FILMS_URL = 'https://www.omdbapi.com/?t=';
 export const getUrlPoster = (filmName) => `${API_POSTER_FILMS_URL}${filmName}&apikey=${API_KEY_FILMS}`;
