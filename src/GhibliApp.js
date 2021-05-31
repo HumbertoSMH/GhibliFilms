a3c12385d89b174fafb99e37f07bb6e13d94ddb0
Humberto Hernández <sasori250393@gmail.com>
2021-05-30 21:46:43 -0500

    Eliminación de Error y Warnings

--- src/GhibliApp.js
+++ src/GhibliApp.js
@@ -4,15 +4,13 @@ import { FilmsContext } from './components/FIlmsContext';
 import { useFetch } from './hooks/useFetchFilms';
 import { AppRouter } from './router/AppRouter';
 
-import GhibliLogo from './studio-ghibli-logo.svg';
-
 export const GhibliApp = () => {
 	const { films, loading } = useFetch();
 
 	return (
 		<>
 			<div className="header">
-				<img src={GhibliLogo} className="header__img" />
+				<img src="https://cdn.worldvectorlogo.com/logos/studio-ghibli-logo.svg" className="header__img" title="logo" />
 			</div>
 			<div className="main">
 				<FilmsContext.Provider value={{ films, loading }}>
